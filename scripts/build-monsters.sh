3bd740a2822d6158279d49a1f0c3e0e34689b6c0
yochaigal <yochaigal@gmail.com>
2022-01-12 19:16:03 -0500

    put monster scripts together

--- scripts/build-monsters.sh
+++ scripts/build-monsters.sh
@@ -1,14 +1,4 @@
----
-layout: default
-title: Website Notes
-search_exclude: true
-nav_exclude: true
----
-
-# Website Notes
-
-### To Compile all Monsters into PDF:
-`#!/bin/bash
+#!/bin/bash
 date="$(date "+%B %e, %Y")"
 rsync -av /home/yochai/github/cairn/resources/monsters/ --exclude=all-monsters.md /tmp/monsters/
 sed -i '/^author/d' /tmp/monsters/*.md
@@ -16,4 +6,3 @@ sed -i '/^source:/d' /tmp/monsters/*.md
 sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' /tmp/monsters/*.md
 pandoc --variable papersize=Letter --variable title="Cairn Monsters" --variable subtitle="Compiled on " --variable subtitle="$date" --variable subtitle=" by Yochai Gal | CC-BY-SA 4.0" --variable mainfont=Alegreya --variable sansfont=Alegreya --variable monofont=Alegreya -f gfm --toc -s /tmp/monsters/*.md -o "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters.pdf"
 rm -rf /tmp/monsters
-`
