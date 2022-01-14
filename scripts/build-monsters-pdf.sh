59b932132ff0b1aad4bbc5143a2693e072853843
Yochai Gal <yochaigal@gmail.com>
2022-01-14 09:06:48 -0500

    updated description

--- scripts/build-monsters-pdf.sh
+++ scripts/build-monsters-pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This creates a single PDF of all the monster stat blocks
+# This creates a compiled PDF of all the monster stat blocks in both A4 and Letter formats (including booklets)
 date="$(date "+%B %e, %Y")"
 rsync -av /home/yochai/github/cairn/resources/monsters/ /tmp/monsters/
 sed -i '/^author/d' /tmp/monsters/*.md
@@ -8,5 +8,5 @@ sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' /tmp/monsters/*.md
 pandoc --variable papersize=Letter --variable title="Cairn Monsters" --variable subtitle="Compiled on " --variable subtitle="$date" --variable subtitle=" by Yochai Gal | CC-BY-SA 4.0" --variable mainfont=Alegreya --variable sansfont=Alegreya --variable monofont=Alegreya -f gfm --toc -s /tmp/monsters/*.md -o "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-letter.pdf"
 pandoc --variable papersize=A4 --variable title="Cairn Monsters" --variable subtitle="Compiled on " --variable subtitle="$date" --variable subtitle=" by Yochai Gal | CC-BY-SA 4.0" --variable mainfont=Alegreya --variable sansfont=Alegreya --variable monofont=Alegreya -f gfm --toc -s /tmp/monsters/*.md -o "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-a4.pdf"
 pdfbook2 -p=letter -s "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-letter.pdf" "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-letter-book.pdf"
-pdfbook2 -p=a4 -s "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-a4.pdf" 
+pdfbook2 -p=a4 -s "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monsters-a4.pdf"
 rm -rf /tmp/monsters
