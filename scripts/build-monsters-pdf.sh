3726b14ee9d9de53454538191942ad529a1b78b5
Yochai Gal <yochaigal@gmail.com>
2022-02-24 13:29:39 -0500

    moved covers to scriptdir

--- scripts/build-monsters-pdf.sh
+++ scripts/build-monsters-pdf.sh
@@ -2,17 +2,17 @@
 # This creates a compiled PDF of all the monster stat blocks in both A4 and Letter formats (including booklets)
 scriptdir="/home/yochai/github/cairn/scripts"
 sourcedir="/home/yochai/github/cairn/resources/monsters"
-tmpdir="/home/yochai/Downloads/tmp/monsters"
+tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
 currentdate="$(date "+%B %e, %Y")"
-mkdir -p $tmpdir
-rsync -av $sourcedir/ $tmpdir/
-sed -i '/^author/d' $tmpdir/*.md
-sed -i '/^source:/d' $tmpdir/*.md
-sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/*.md
+mkdir -p $tmpdir/monsters
+rsync -av $sourcedir/ $tmpdir/monsters/
+sed -i '/^author/d' $tmpdir/monsters/*.md
+sed -i '/^source:/d' $tmpdir/monsters/*.md
+sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/monsters/*.md
 
 # Create the PDF
-pandoc  -s $tmpdir/*.md \
+pandoc  -s $tmpdir/monsters/*.md \
         -V papersize=Letter \
         -V title="Cairn Bestiary" \
         -V subtitle="Compiled on " \
@@ -23,13 +23,13 @@ pandoc  -s $tmpdir/*.md \
         --metadata=title:"Cairn Bestiary" \
         --metadata=author:"Yochai Gal" \
         --metadata=lang:"en-US" \
-        --metadata=cover-image:"$scriptdir/covers/cairn-monsters-letter-front-cover.png" \
+        --metadata=cover-image:"$scriptdir/covers/cairn-monsters-front-cover.png" \
         -f gfm \
         --toc \
         -H head.tex \
-        -o $tmpdir/cairn-monsters-letter-tmp.pdf
+        -o $tmpdir/monsters/cairn-monsters-letter-tmp.pdf
 
-pandoc  -s $tmpdir/*.md \
+pandoc  -s $tmpdir/monsters/*.md \
         -V papersize=A4 \
         -V title="Cairn Bestiary" \
         -V subtitle="Compiled on " \
@@ -44,15 +44,15 @@ pandoc  -s $tmpdir/*.md \
         -f gfm \
         --toc \
         -H head.tex \
-        -o $tmpdir/cairn-monsters-a4-tmp.pdf
+        -o $tmpdir/monsters/cairn-monsters-a4-tmp.pdf
 
 # Add covers
-pdftk "$destdir/covers/letter/cairn-monsters-letter-front-cover.pdf" $tmpdir/cairn-monsters-letter-tmp.pdf "$destdir/covers/letter/cairn-monsters-letter-back-cover.pdf" cat output "$destdir/cairn-monsters-letter.pdf"
-pdftk "$destdir/covers/a4/cairn-monsters-a4-front-cover.pdf" $tmpdir/cairn-monsters-a4-tmp.pdf "$destdir/covers/a4/cairn-monsters-a4-back-cover.pdf" cat output "$destdir/cairn-monsters-a4.pdf"
+pdftk "$scriptdir/covers/letter/cairn-monsters-letter-front-cover.pdf" $tmpdir/monsters/cairn-monsters-letter-tmp.pdf "$scriptdir/covers/letter/cairn-monsters-letter-back-cover.pdf" cat output "$destdir/cairn-monsters-letter.pdf"
+pdftk "$scriptdir/covers/a4/cairn-monsters-a4-front-cover.pdf" $tmpdir/monsters/cairn-monsters-a4-tmp.pdf "$scriptdir/covers/a4/cairn-monsters-a4-back-cover.pdf" cat output "$destdir/cairn-monsters-a4.pdf"
 
 # Make the booklet
-pdfbook2 --paper=letter -s $tmpdir/cairn-monsters-letter-tmp.pdf
-pdfbook2 --paper=a4paper -s $tmpdir/cairn-monsters-a4-tmp.pdf
-pdftk "$destdir/covers/letter/cairn-monsters-letter-covers-landscape.pdf" $tmpdir/cairn-monsters-letter-tmp-book.pdf cat output "$destdir/cairn-monsters-letter-booklet.pdf"
-pdftk "$destdir/covers/a4/cairn-monsters-a4-covers-landscape.pdf" $tmpdir/cairn-monsters-a4-tmp-book.pdf cat output "$destdir/cairn-monsters-a4-booklet.pdf"
+pdfbook2 --paper=letter -s $tmpdir/monsters/cairn-monsters-letter-tmp.pdf
+pdfbook2 --paper=a4paper -s $tmpdir/monsters/cairn-monsters-a4-tmp.pdf
+pdftk "$scriptdir/covers/letter/cairn-monsters-letter-covers-landscape.pdf" $tmpdir/monsters/cairn-monsters-letter-tmp-book.pdf cat output "$destdir/cairn-monsters-letter-booklet.pdf"
+pdftk "$scriptdir/covers/a4/cairn-monsters-a4-covers-landscape.pdf" $tmpdir/monsters/cairn-monsters-a4-tmp-book.pdf cat output "$destdir/cairn-monsters-a4-booklet.pdf"
 rm -rf $tmpdir
