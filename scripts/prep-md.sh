db3650595cf73055f65b9cc1a3fd3137ec33fae2
Yochai Gal <yochaigal@gmail.com>
2022-02-24 14:12:47 -0500

    updated staging script

--- scripts/prep-md.sh
+++ scripts/prep-md.sh
@@ -2,16 +2,18 @@
 # This creates a compiled PDF of all the monster stat blocks in both A4 and Letter formats (including booklets)
 scriptdir="/home/yochai/github/cairn/scripts"
 sourcedir="/home/yochai/github/cairn/resources/monsters"
-tmpdir="/tmp/monsters"
+tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
 currentdate="$(date "+%B %e, %Y")"
-rsync -av $sourcedir/ $tmpdir/
-sed -i '/^author/d' $tmpdir/*.md
-sed -i '/^source:/d' $tmpdir/*.md
-sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/*.md
+#mkdir -p $tmpdir/monsters
+#rsync -av $sourcedir/ $tmpdir/monsters/
+#sed -i '/^author/d' $tmpdir/monsters/*.md
+#sed -i '/^source:/d' $tmpdir/monsters/*.md
+#sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/monsters/*.md
+
 
 # Create the PDF
-pandoc  -s $tmpdir/*.md \
+pandoc  -s $tmpdir/monsters/*.md \
         -V papersize=Letter \
         -V title="Cairn Bestiary" \
         -V subtitle="Compiled on " \
@@ -22,16 +24,11 @@ pandoc  -s $tmpdir/*.md \
         --metadata=title:"Cairn Bestiary" \
         --metadata=author:"Yochai Gal" \
         --metadata=lang:"en-US" \
-        --metadata=cover-image:"$scriptdir/covers/cairn-monsters-letter-front-cover.png" \
+        --metadata=cover-image:"$scriptdir/covers/cairn-monsters-front-cover.png" \
         -f gfm \
         --toc \
         -H head.tex \
-        -o $tmpdir/cairn-monsters-letter-tmp.pdf
+        -o $tmpdir/monsters/cairn-monsters-letter-tmp.pdf
 
-#-A foot.tex \
-#-B body.tex \
-#-V toc-own-page="true" \
-#--pdf-engine=lualatex \
-#--toc-depth=5 \
-#sed -i '$a \\\end{samepage}\n' $tmpdir/*.md
-#sed -i '1 s/^/\\begin{samepage}\n/' $tmpdir/*.md
+#sed -i '1 s/^/\\begin{document}\n/' $tmpdir/monsters/*.md
+#sed -i '$a \\\end{document}\n' $tmpdir/monsters/*.md
