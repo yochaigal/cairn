daaa998b8027a75533d5c09cc3dde75d1dfa94c1
yochaigal <yochaigal@gmail.com>
2022-02-26 22:07:11 -0500

    still bangin' away

--- scripts/prep-md.sh
+++ scripts/prep-md.sh
@@ -5,22 +5,22 @@ sourcedir="/home/yochai/github/cairn/resources/monsters"
 tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
 currentdate="$(date "+%B %e, %Y")"
-mkdir -p $tmpdir/monsters
-rsync -av $sourcedir/ $tmpdir/monsters/
-sed -i '/^author/d' $tmpdir/monsters/*.md
-sed -i '/^source:/d' $tmpdir/monsters/*.md
-sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/monsters/*.md
+#mkdir -p $tmpdir/monsters
+#rsync -av $sourcedir/ $tmpdir/monsters/
+#sed -i '/^author/d' $tmpdir/monsters/*.md
+#sed -i '/^source:/d' $tmpdir/monsters/*.md
+#sed -i '1 { /^---/ { :a N; /\n---/! ba; d} }' $tmpdir/monsters/*.md
 
 # Create the PDF
 pandoc  -s $tmpdir/monsters/*.md \
         -f gfm \
         --toc \
-        --H monsters-cover.tex \
+        -H monsters-cover.tex \
         -V papersize=Letter \
         -V title="Cairn Bestiary" \
         -V subtitle="Compiled on " \
         -V subtitle="$currentdate" \
-        -V subtitle=" by Yochai Gal | CC-BY-SA 4.0" \
+        -V subtitle=" | CC-BY-SA 4.0" \
         -V fontfamily="Alegreya" \
         -V fontsize=12pt \
         --metadata=title:"Cairn Bestiary" \
