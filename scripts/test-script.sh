8a286e52e092876fc5320d1421ce52d6078b93f9
yochaigal <yochaigal@gmail.com>
2022-07-10 08:27:07 -0400

    testing something

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -5,9 +5,9 @@ sourcedir="/home/yochai/github/cairn/resources/monsters"
 tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
 currentdate="$(date "+%B %e, %Y")"
-#mkdir -p $tmpdir/monsters
-#rsync -av $sourcedir/ $tmpdir/monsters/
-#sed -i -f clean.sed $tmpdir/monsters/*.md
+mkdir -p $tmpdir/monsters
+rsync -av $sourcedir/ $tmpdir/monsters/
+sed -i -f clean.sed $tmpdir/monsters/*.md
 #sed -i -f prep.sed $tmpdir/monsters/*.md
 
 # Create the PDF
