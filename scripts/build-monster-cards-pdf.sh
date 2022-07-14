d4947ed7ed4ad01f16f9e2417c101f4028d8e4d9
yochaigal <yochaigal@gmail.com>
2022-07-14 17:25:57 -0400

    renamed python source

--- scripts/build-monster-cards-pdf.sh
+++ scripts/build-monster-cards-pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This creates a printable PDF of all the monster cards in both Letter and A4 formats.
-montage "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/monster-cards/*.png" -mode concatenate -tile 3x3 -geometry +20+20 -page letter -rotate 90 "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monster-cards-letter.pdf"
-montage "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/monster-cards/*.png" -mode concatenate -tile 3x3 -geometry +20+20 -page a4 -rotate 90 "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-monster-cards-a4.pdf"
+montage "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/monster-cards/*.png" -mode concatenate -tile 3x3 -geometry +20+20 -page letter -rotate 90 "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-bestiary-cards-letter.pdf"
+montage "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/monster-cards/*.png" -mode concatenate -tile 3x3 -geometry +20+20 -page a4 -rotate 90 "/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-bestiary-cards-a4.pdf"
