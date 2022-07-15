6684e9c783b702d504c3d1cf02fe7d436ce59f87
yochaigal <yochaigal@gmail.com>
2022-07-14 21:02:23 -0400

    still pokin' around

--- scripts/test-lulu.sh
+++ scripts/test-lulu.sh
@@ -5,30 +5,29 @@ sourcedir="/home/yochai/github/cairn/resources/monsters"
 tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
 currentdate="$(date "+%B %e, %Y")"
-#mkdir -p $tmpdir/monsters
-#rsync -av $sourcedir/ $tmpdir/monsters/
-#sed -i -f prep.sed $tmpdir/monsters/*.md
-#cp sources/template.md $tmpdir/all-monsters.md
-#cat $tmpdir/monsters/*.md > $tmpdir/all-monsters.md
+cp sources/frame.tex $tmpdir/cairn-bestiary.tex
+mkdir -p $tmpdir/monsters
+rsync -av $sourcedir/ $tmpdir/monsters/
+sed -i -f prep.sed $tmpdir/monsters/*.md
+cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary.md
+pdflatex $tmpdir/cairn-bestiary.tex -output-directory=$tmpdir
 
 # Create the PDF
-pandoc  -s $tmpdir/all-monsters.md \
-        -t latex \
-        --toc \
-   	--template=sources/build.tex \
-        --metadata=title:"Cairn Bestiary" \
-        --metadata=author:"Yochai Gal" \
-        --metadata=lang:"en-US" \
-        --metadata=cover-image:$scriptdir/covers/cairn-bestiary-front-cover.png \
-        -V geometry:papersize="{5.7in,8.7in}"  \
-        -V geometry:margin=.5in \
-	-V title="Cairn Bestiary" \
-        -V subtitle="Compiled on " \
-        -V subtitle="$currentdate" \
-        -V subtitle=" by Yochai Gal | CC-BY-SA 4.0" \
-        -V fontfamily="Alegreya" \
-        -V fontsize=10pt \
-	-o $tmpdir/all-monsters.tex
+#pandoc  -s $tmpdir/cairn-bestiary.md \
+#        -t latex \
+#        --toc \
+#   	--template=sources/build.tex \
+#        --metadata=title:"Cairn Bestiary" \
+#        --metadata=author:"Yochai Gal" \
+#        --metadata=lang:"en-US" \
+#        --metadata=cover-image:$scriptdir/covers/cairn-bestiary-front-cover.png \
+#        -V geometry:papersize="{5.7in,8.7in}"  \
+#        -V geometry:margin=.5in \
+#	-V title="Cairn Bestiary" \
+#        -V subtitle="Compiled on " \
+#        -V subtitle="$currentdate" \
+#        -V subtitle=" by Yochai Gal | CC-BY-SA 4.0" \
+#        -V fontfamily="Alegreya" \
+#        -V fontsize=10pt \
+#	-o $tmpdir/cairn-bestiary.tex
 
-
-# -o "$destdir"/cairn-bestiary-lulu.pdf
