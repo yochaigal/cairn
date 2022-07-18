0875476a8cbc0b0e4a147c48daacbb5c1d59ec7c
Yochai Gal <yochaigal@gmail.com>
2022-07-18 11:19:43 -0400

    Lulu working at 48 pages.

--- scripts/build-lulu-pdf.sh
+++ scripts/build-lulu-pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This creates a compiled PDF of all the monster stat blocks in both A4 and Letter formats (including booklets)
+# This creates the interior files for the lulu print option
 scriptdir="/home/yochai/github/cairn/scripts"
 sourcedir="/home/yochai/github/cairn/resources/monsters"
 tmpdir="/home/yochai/Downloads/tmp"
@@ -9,7 +9,7 @@ mkdir -p $tmpdir/monsters
 rsync -av $sourcedir/ $tmpdir/monsters/
 sed -i -f prep.sed $tmpdir/monsters/*.md
 cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary-tmp.md
-cp sources/lulu.latex $tmpdir/cairn-bestiary.tex
+cp sources/lulu.tex $tmpdir/cairn-bestiary.tex
 pandoc $tmpdir/cairn-bestiary-tmp.md -f markdown -t latex -o $tmpdir/cairn-bestiary-tmp.tex
 cat $tmpdir/cairn-bestiary-tmp.tex >> $tmpdir/cairn-bestiary.tex
 sed -i '$a \\\end{document}' $tmpdir/cairn-bestiary.tex
