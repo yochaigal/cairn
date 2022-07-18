f3b98fc3c5bb47fafb470447214e4a89c7c9843b
Yochai Gal <yochaigal@gmail.com>
2022-07-18 15:46:48 -0400

    fixed script names again (sigh)

--- scripts/build-bestiary-master-script.sh
+++ scripts/build-bestiary-master-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# creates compiled monster PDFs (letter/A4) and booklets, then monster cards (PNG) and PDFs (Letter/A4)
+# creates compiled monster PDFs (letter/A4) and booklets, then lulu-ready PDF, then monster cards (PNG) and PDFs (Letter/A4)
 sh /home/yochai/github/cairn/scripts/build-bestiary-letter.sh
 sh /home/yochai/github/cairn/scripts/build-bestiary-a4.sh
 sh /home/yochai/github/cairn/scripts/build-bestiary-lulu.sh
-python3 /home/yochai/github/cairn/scripts/bestiary_cards.py
+python3 /home/yochai/github/cairn/scripts/build-bestiary-cards-images.py
 sh /home/yochai/github/cairn/scripts/build-bestiary-cards-pdf.sh
