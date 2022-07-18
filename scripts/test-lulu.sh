33e25ed35495e29f42926f8a6c839c710b9099c1
Yochai Gal <yochaigal@gmail.com>
2022-07-18 10:48:52 -0400

    lulu script ready

--- scripts/test-lulu.sh
+++ scripts/test-lulu.sh
@@ -4,12 +4,15 @@ scriptdir="/home/yochai/github/cairn/scripts"
 sourcedir="/home/yochai/github/cairn/resources/monsters"
 tmpdir="/home/yochai/Downloads/tmp"
 destdir="/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters"
-currentdate="$(date "+%B %e, %Y")"
-cp sources/frame.tex $tmpdir/cairn-bestiary.tex
 mkdir -p $tmpdir/monsters
 rsync -av $sourcedir/ $tmpdir/monsters/
 sed -i -f prep.sed $tmpdir/monsters/*.md
-cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary.md
-pandoc $tmpdir/cairn-bestiary.md -f markdown -t latex -o $tmpdir/cairn-bestiary.tex
-pdflatex $tmpdir/cairn-bestiary.md
+cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary-tmp.md
+cp sources/lulu.latex $tmpdir/cairn-bestiary.tex
+pandoc $tmpdir/cairn-bestiary-tmp.md -f markdown -t latex -o $tmpdir/cairn-bestiary-tmp.tex
+cat $tmpdir/cairn-bestiary-tmp.tex >> $tmpdir/cairn-bestiary.tex
+sed -i '$a \\\end{document}' $tmpdir/cairn-bestiary.tex
+pdflatex -aux-directory=$tmpdir -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
+pdflatex -aux-directory=$tmpdir -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
+
 
