a71dd3ba676d0b36c76f1724e71d9e0098425a2b
Yochai Gal <yochaigal@gmail.com>
2023-04-27 09:08:25 -0400

    fixed scripts

--- scripts/build-bestiary-a5.sh
+++ scripts/build-bestiary-a5.sh
@@ -10,7 +10,7 @@ cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary-tmp.md
 cp sources/a5.tex $tmpdir/cairn-bestiary.tex
 pandoc $tmpdir/cairn-bestiary-tmp.md -f markdown -t latex -o $tmpdir/cairn-bestiary-tmp.tex
 cat $tmpdir/cairn-bestiary-tmp.tex >> $tmpdir/cairn-bestiary.tex
-sed -i '$a \newpage\null\thispagestyle{empty}\newpage' $tmpdir/cairn-bestiary.tex
+sed -i '$a \\\newpage\\null\\thispagestyle{empty}\\newpage' $tmpdir/cairn-bestiary.tex
 sed -i '$a \\\end{document}' $tmpdir/cairn-bestiary.tex
 pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
 pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex
