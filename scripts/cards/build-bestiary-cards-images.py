38bd39c0deabedf22d59c15d116109c10c98d6ba
Yochai Gal <yochaigal@gmail.com>
2024-01-11 16:26:47 -0500

    updated harpy

--- scripts/cards/build-bestiary-cards-images.py
+++ scripts/cards/build-bestiary-cards-images.py
@@ -1,4 +1,6 @@
-# -*- coding: UTF-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # Written by Oskar Swida and Yochai Gal
 # This creates monster cards from original monsters in markdown format
 
@@ -12,7 +14,14 @@ from PIL import Image, ImageDraw, ImageFont
 
 
 class Coords:
-    def __init__(self, x, y, width, height):
+
+    def __init__(
+        self,
+        x,
+        y,
+        width,
+        height,
+        ):
         self.x = x
         self.y = y
         self.width = width
@@ -20,63 +29,85 @@ class Coords:
 
 
 class TextBox:
+
     def __init__(self, coords):
         self.coords = coords
 
-    def compute_font_size(self, text, font_name, initial_size=64):
+    def compute_font_size(
+        self,
+        text,
+        font_name,
+        initial_size=64,
+        ):
         fnt = ImageFont.truetype(font_name, initial_size)
         fs = initial_size
         fsize = fnt.getsize_multiline(text)
-        if fsize[0] < self.coords.width and fsize[1] < self.coords.height:
-            return fs, text.splitlines()
+        if fsize[0] < self.coords.width and fsize[1] \
+            < self.coords.height:
+            return (fs, text.splitlines())
         lines = text.splitlines()
         outl = []
-        while fsize[0] >= self.coords.width or fsize[1] >= self.coords.height:
+        while fsize[0] >= self.coords.width or fsize[1] \
+            >= self.coords.height:
             fs = fs - 1
             fnt = ImageFont.truetype(font_name, fs)
-            cw = fnt.getsize("o")
-            line_width = int(self.coords.width/cw[0])+1
+            cw = fnt.getsize('o')
+            line_width = int(self.coords.width / cw[0]) + 1
             outl = []
             for l in lines:
-                wrapped = wrap(
-                    l, line_width, replace_whitespace=False, break_long_words=True)
+                wrapped = wrap(l, line_width, replace_whitespace=False,
+                               break_long_words=True)
                 for x in wrapped:
                     outl.append(x)
-            fsize = fnt.getsize_multiline("\n".join(outl))
+            fsize = fnt.getsize_multiline('\n'.join(outl))
 
-        return fs, outl
+        return (fs, outl)
 
-    def drawText(self, image, text, font_name, center=False, angle=0, initial_size=64):
+    def drawText(
+        self,
+        image,
+        text,
+        font_name,
+        center=False,
+        angle=0,
+        initial_size=64,
+        ):
         canvas = ImageDraw.Draw(image)
-        fs, lines = self.compute_font_size(
-            text, font_name=font_name, initial_size=initial_size)
+        (fs, lines) = self.compute_font_size(text, font_name=font_name,
+                initial_size=initial_size)
         fnt = ImageFont.truetype(font_name, fs)
         cnt = 0
-        ch = fnt.getsize("gh")
-        fsize = fnt.getsize_multiline("\n".join(lines))
+        ch = fnt.getsize('gh')
+        fsize = fnt.getsize_multiline('\n'.join(lines))
         if angle != 0:
-            img_txt = Image.new('L', fsize, color=(255))
+            img_txt = Image.new('L', fsize, color=255)
             draw_txt = ImageDraw.Draw(img_txt)
             for l in lines:
                 isize = fnt.getsize(l)
+
                 # experimental results
-                y = cnt*ch[1]*0.75 - ch[1]/6
+
+                y = cnt * ch[1] * 0.75 - ch[1] / 6
                 if center:
-                    x = (fsize[0] - isize[0])/2
+                    x = (fsize[0] - isize[0]) / 2
                 else:
                     x = 0
-                draw_txt.text((x, y), l, "black", font=fnt)
+                draw_txt.text((x, y), l, 'black', font=fnt)
                 cnt = cnt + 1
             img_txt = img_txt.rotate(angle, expand=1)
-            sx, sy = img_txt.size
+            (sx, sy) = img_txt.size
+
             # in fact only for 90 degree angle, require computation
-            y = self.coords.y+int((self.coords.width-fsize[0])/2)
-            image.paste(img_txt, (self.coords.x, y, self.coords.x+sx, y+sy))
+
+            y = self.coords.y + int((self.coords.width - fsize[0]) / 2)
+            image.paste(img_txt, (self.coords.x, y, self.coords.x + sx,
+                        y + sy))
         else:
             for l in lines:
-                y = self.coords.y + cnt*ch[1]*0.8
+                y = self.coords.y + cnt * ch[1] * 0.8
                 if center:
-                    x = self.coords.x + (self.coords.width - fsize[0])/2
+                    x = self.coords.x + (self.coords.width - fsize[0]) \
+                        / 2
                 else:
                     x = self.coords.x
                 canvas.text((x, y), l, (0, 0, 0), font=fnt)
@@ -84,6 +115,7 @@ class TextBox:
 
 
 class MonsterCard:
+
     def __init__(self, outdir):
         self.titleCoords = Coords(19, 79, 314, 67)
         self.titleBox = TextBox(self.titleCoords)
@@ -94,44 +126,54 @@ class MonsterCard:
         self.outdir = outdir
 
     def generate(self, mdfile):
-        self.image = Image.open("/home/yochai/github/cairn/scripts/sources/monster-ls.png")
-        text_file = open(mdfile, "r", encoding="utf-8")
+        self.image = \
+            Image.open('/home/yochai/github/cairn/scripts/sources/monster-ls.png'
+                       )
+        text_file = open(mdfile, 'r', encoding='utf-8')
         data = text_file.read()
         text_file.close()
         res = marko.convert(data)
+
         # replace formatting because xpath engine seems not to respect descendant axis
-        res = res.replace("<em>", "", -1).replace("</em>", "", -1)
+
+        res = res.replace('<em>', '', -1).replace('</em>', '', -1)
         tree = html.fromstring(res)
         title = tree.xpath('//h1/text()')
         if title == None or len(title) == 0:
-            print("PANIC! Cannot find title in", data)
+            print ('PANIC! Cannot find title in', data)
             return
         paragraphs = tree.xpath('//p/text()')
         if paragraphs == None or len(paragraphs) == 0:
-            print("PANIC! Cannot find traits line in", data)
+            print ('PANIC! Cannot find traits line in', data)
             return
         desclines = tree.xpath('//ul/li/text()')
         if desclines == None or len(desclines) == 0:
-            print("PANIC! Cannot find description list in", data)
+            print ('PANIC! Cannot find description list in', data)
             return
         desc = []
         for d in desclines:
-            d = d.replace("\n", " ", -1)
-            desc.append("• "+d)
-        self.attrBox.drawText(
-            self.image, paragraphs[0], '/home/yochai/github/cairn/scripts/fonts/Alegreya-Italic.ttf', center=True, initial_size=56)
-        self.titleBox.drawText(
-            self.image, title[0], '/home/yochai/github/cairn/scripts/fonts/Alegreya-Bold.ttf', angle=90, center=True)
-        self.descBox.drawText(self.image, "\n".join(
-            desc), '/home/yochai/github/cairn/scripts/fonts/Alegreya-Regular.ttf', initial_size=40)
-        self.image.save(self.outdir+"/"+title[0]+".png")
+            d = d.replace('\n', ' ', -1)
+            desc.append("• " + d)
+        self.attrBox.drawText(self.image, paragraphs[0],
+                              '/home/yochai/github/cairn/scripts/fonts/Alegreya-Italic.ttf'
+                              , center=True, initial_size=56)
+        self.titleBox.drawText(self.image, title[0],
+                               '/home/yochai/github/cairn/scripts/fonts/Alegreya-Bold.ttf'
+                               , angle=90, center=True)
+        self.descBox.drawText(self.image, '\n'.join(desc),
+                              '/home/yochai/github/cairn/scripts/fonts/Alegreya-Regular.ttf'
+                              , initial_size=40)
+        self.image.save(self.outdir + '/' + title[0] + '.png')
         pass
 
 
 # Please adjust to your monster markdown & output directory
-dir = "/home/yochai/github/cairn/resources/monsters"
-filelist = [f for f in glob.glob(dir + "/*.md")]
+
+dir = '/home/yochai/github/cairn/resources/monsters'
+filelist = [f for f in glob.glob(dir + '/*.md')]
 cnt = 0
 for file in filelist:
-    mc = MonsterCard("/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-bestiary-cards")
+    mc = \
+        MonsterCard('/home/yochai/Google Drive/Games/OSR/Into The Odd/hacks/Cairn/Monsters/cairn-bestiary-cards'
+                    )
     mc.generate(file)
