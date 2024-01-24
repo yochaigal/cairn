9a9270414785a0ddda9e510180284e0127290c4c
Yochai Gal <yochaigal@gmail.com>
2024-01-24 11:53:31 -0500

    fixed some silly stuff

--- scripts/build-bestiary-letter.sh
+++ scripts/build-bestiary-letter.sh
@@ -10,10 +10,11 @@ cat $tmpdir/monsters/*.md >> $tmpdir/cairn-bestiary-tmp.md
 cp sources/letter.tex $tmpdir/cairn-bestiary.tex
 pandoc $tmpdir/cairn-bestiary-tmp.md -f markdown -t latex -o $tmpdir/cairn-bestiary-tmp.tex
 cat $tmpdir/cairn-bestiary-tmp.tex >> $tmpdir/cairn-bestiary.tex
+sed -i '$a \\\newpage\\null\\thispagestyle{empty}\\newpage' $tmpdir/cairn-bestiary.tex
 sed -i '$a \\\end{document}' $tmpdir/cairn-bestiary.tex
 pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex 
 pdflatex -output-directory=$tmpdir $tmpdir/cairn-bestiary.tex
-pdfjam --letterpaper --booklet true --landscape --noautoscale true "$tmpdir/cairn-bestiary.pdf" -o "$tmpdir/cairn-bestiary-letter-booklet.pdf" --preamble '\usepackage{everyshi} \makeatletter \EveryShipout{\ifodd\c@page\pdfpageattr{/Rotate 180}\fi} \makeatother'
+pdfjam --letterpaper --booklet true --landscape "$tmpdir/cairn-bestiary.pdf" -o "$tmpdir/cairn-bestiary-booklet.pdf" --preamble '\usepackage{everyshi} \makeatletter \EveryShipout{\ifodd\c@page\pdfpageattr{/Rotate 180}\fi} \makeatother'
 mv $tmpdir/cairn-bestiary.pdf "$destdir/cairn-bestiary-letter.pdf"
-mv $tmpdir/cairn-bestiary-letter-booklet.pdf "$destdir/cairn-bestiary-letter-booklet.pdf"
+mv $tmpdir/cairn-bestiary-booklet.pdf "$destdir/cairn-bestiary-letter-booklet.pdf"
 rm -rf $tmpdir
